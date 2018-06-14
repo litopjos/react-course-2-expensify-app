63ca1c76bbfbbb746895f7b23605a6e3180ca367
litopjos <joselitope@hotmail.com>
2018-06-14 19:34:32 +0800

    changes for amount and date format

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -2,15 +2,19 @@ import React from "react";
 import {connect} from "react-redux";
 import {removeExpense} from "../actions/actions-expenses";
 import {Link} from "react-router-dom";
+import moment from "moment";
+import numeral from "numeral"
 
 const ExpenseListItem = ({dispatch,id,description,amount,createAt},props)=>(
     <div>
         <Link to={`/edit/${id}`}>
         <h3>{description} </h3>
         </Link>
-        <p>{amount}-{createAt}</p>
-
-
+        <p>
+            {numeral(amount/100).format('$0,0.00')}
+            -
+            {moment(createAt).format("MMMM Do, YYYY")};
+        </p>
     </div>
 
 );
